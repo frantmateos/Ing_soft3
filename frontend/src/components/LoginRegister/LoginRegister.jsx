a72cc07006b8c190d8397153fa716247a58e08e9
Francisco Mateos <102611148+frantmateos@users.noreply.github.com>
2025-11-24 18:22:07 -0300

    Update login header text to 'Login Administradores V2'

--- frontend/src/components/LoginRegister/LoginRegister.jsx
+++ frontend/src/components/LoginRegister/LoginRegister.jsx
@@ -84,7 +84,7 @@ const LoginRegister = () => {
     <div className={`wrapper ${action}`}>
       <div className="from-box login">
         <form onSubmit={handleSubmitLogin}>
-          <h1>Login Administradores PRO</h1>
+          <h1>Login Administradores V2</h1>
           <div className="input-box">
             <input 
               type="text" 
